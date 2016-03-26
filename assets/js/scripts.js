0ca140bf081f321d9ca1a9ba1d354ca7c8522630
Slesarev Sergei <slesarev071290@gmail.com>
2016-03-26 23:57:45 +0200

    + fonts EI, Chrome, FireFox

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,54 +1,30 @@
 $(document).ready(function() {
-	//mixItUp
-	// $(function(){
-	// 	// Instantiate MixItUp:
-	// 	$('#container').mixItUp();
-	// });
 
 	//Slick Carousel
 	$('#js-slider').slick({
-		//cssEase: 'ease',
-		//fade: true,  // Cause trouble if used slidesToShow: more than one
 		arrows: false,
 		dots: true,
 		infinite: true,
-		//speed: 500,
-		//autoplay: true,
-		//autoplaySpeed: 3000,
 		slidesToShow: 1,
 		slidesToScroll: 1
 	});
 
+	// Remove placeholder
+	$("input,textarea").each(function() {
+		
+		$(this).data('holder',$(this).attr('placeholder'));
 
-	jQuery("input,textarea").each(function() {
-		jQuery(this).data('holder',jQuery(this).attr('placeholder'));
-
-		jQuery(this).focusin(function(){
-			jQuery(this).attr('placeholder','');
+		$(this).focusin(function(){
+			$(this).attr('placeholder','');
 		});
 
-		jQuery(this).focusout(function(){
-			jQuery(this).attr('placeholder',jQuery(this).data('holder'));
+		$(this).focusout(function(){
+			$(this).attr('placeholder',$(this).data('holder'));
 		});
+
 	});
 
-	// external js: masonry.pkgd.js, imagesloaded.pkgd.js
-
-	// var grid = document.querySelector('.grid');
-	// var msnry;
-
-	// imagesLoaded( grid, function() {
-	//   // init Isotope after all images have loaded
-	//   msnry = new Masonry( grid, {
-	//   	itemSelector: '.grid-item',
-	//   	columnWidth: '.grid-sizer',
-	//   	fitWidth: true
-	//   	//percentPosition: true,
-	//   	// originLeft: false,
-	//   	//originTop: false
-	//   });
-	// });
-
+	// Masonry
 	$('.grid').isotope({
 	  itemSelector: '.grid-item',
 	  percentPosition: true,
@@ -57,6 +33,11 @@ $(document).ready(function() {
 	    columnWidth: '.grid-sizer'
 	  }
 	})
+
+	// Scroll Top
+	$( ".b-footer__imgUp").click(function(){
+		$('html, body').animate({scrollTop: 0}, 'slow');
+	});
 
 });
 
